33a8fd373c8e8e6459400f741053c8d1529c9fef
sunilpaulmathew <sunil.kde@gmail.com>
2018-12-14 13:40:41 +0900

    SmartPack boot script: update injection method credits to osm0sis & eng.stk
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- anykernel_SmartPack/anykernel.sh
+++ anykernel_SmartPack/anykernel.sh
@@ -67,8 +67,6 @@ dump_boot;
 # begin ramdisk changes
 
 # init.rc
-backup_file init.rc;
-grep "import /init.SmartPack.rc" init.rc >/dev/null || sed -i '1,/.*import.*/s/.*import.*/import \/init.SmartPack.rc\n&/' init.rc
 
 # init.tuna.rc
 
